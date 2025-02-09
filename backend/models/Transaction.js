31708c837a8fd62976f5201c8eeb98fd35ee371a
Calvin Secrest <dev@calvinsecrest.com>
2025-02-09 14:52:55 -0500

    Create Transaction.js

---
+++ backend/models/Transaction.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  contractId: { type: mongoose.Schema.Types.ObjectId, ref: "Contract" },
+  consumerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  producerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  rating: { type: Number, min: -1, max: 4 }, // LBTAS rating system
+  status: { type: String, enum: ["pending", "completed"], default: "pending" },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
