3567298afa677e8f1af5b54e8a91dcabef4e883e
Calvin Secrest <dev@calvinsecrest.com>
2025-03-26 20:36:18 -0400

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,10 @@
 const Notification = require("../models/Notification");
+const { addPingJob } = require('../bull/pingJobs');
 
 await Notification.create({
   userId: transaction.buyerId,
   message: `Your transaction ${transaction._id} has been initiated.`
 });
+
+await newTransaction.save();
+addPingJob(newTransaction._id);
